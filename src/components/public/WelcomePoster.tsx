d0886e01aa79416e03615e83dcd5ae0c6f121b72
bohemioe2 <eder.beltran.acosta@gmail.com>
2026-04-14 14:20:30 -0600

    chore: revert CSS mix-blend-screen as user design workflow was identified

--- src/components/public/WelcomePoster.tsx
+++ src/components/public/WelcomePoster.tsx
@@ -173,9 +173,7 @@ export default function WelcomePoster({ folio, name, eventName, category, photoU
     }
 
     if (templateImg) {
-      ctx.globalCompositeOperation = "screen";
       ctx.drawImage(templateImg, 0, 0, W, H);
-      ctx.globalCompositeOperation = "source-over"; // Reset
     }
 
     if (logoImg) {
@@ -447,7 +445,7 @@ export default function WelcomePoster({ folio, name, eventName, category, photoU
           <img 
             src={posterTemplateUrl} 
             alt="Template" 
-            className="absolute inset-0 w-full h-full object-cover z-10 pointer-events-none mix-blend-screen" 
+            className="absolute inset-0 w-full h-full object-cover z-10 pointer-events-none" 
           />
         )}
 
